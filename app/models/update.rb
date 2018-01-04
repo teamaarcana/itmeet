9f0aeee37f037b4218768c9679723979b60f1026
deepakkarna23 <deepakkarna23@gmail.com>
2018-01-04 15:14:08 +0545

    update controller and model created

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,2 +1,3 @@
 class Update < ApplicationRecord
+  
 end
--- db/migrate/20180104084535_create_updates.rb
+++ db/migrate/20180104084535_create_updates.rb
@@ -1,7 +1,7 @@
 class CreateUpdates < ActiveRecord::Migration[5.1]
   def change
     create_table :updates do |t|
-
+      
       t.timestamps
     end
   end
